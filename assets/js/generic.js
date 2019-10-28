27b7397d43a3d885dd566dc17edcf978c4c1900e
Tibor den Held <tibor.denheld@ns.nl>
2019-10-28 09:28:36 +0100

    removed jQuery from generic

--- assets/js/generic.js
+++ assets/js/generic.js
@@ -1,68 +1,125 @@
-// basic functions
-// ------------------------------------------------------------
-function toggle() {
-    const obj = $('.js-toggle');
-    if (obj[0]) {
-        obj.click(function () {
-            $(this).toggleClass('is-active');
-        });
-    }
+const exists = node => {
+    return document.body.contains(document.querySelector(node));
 }
 
-function clearSession() {
-    const obj = $('.js-clear-session');
-    if (obj[0]) {
-        obj.click(function () {
-            sessionStorage.clear();
-        });
-    }
+const isHidden = node => {
+    return window.getComputedStyle(node).display === 'none';
 }
 
-function button() {
-    const obj = $('.button');
-    if (obj[0]) {
-        obj.click(function () {
-            if ($(this).attr('data-loader')) {
-                $(this).toggleClass('is-loading');
-            }
-        });
-    }
+const valueInArray = (value, array) => {
+    if (array.includes(value)) return true;
 }
 
-$(function () {
+const removeAllChilds = node => {
+    while (node.firstChild) node.removeChild(node.firstChild);
+}
+
+const getHeight = node => {
+    node.style.display = 'block';
+    node.style.visibility = 'hidden';
+    node.style.transition = '';
+    const height = node.offsetHeight + 'px';
+    node.style.display = '';
+    node.style.visibility = '';
+    return height;
+}
+
+const slideDown = (node, duration, d, callback) => {
+    if (!isHidden(node)) return;
+    const display = d || 'block';
+    const height = getHeight(node);
+
+    node.style.display = display;
+    node.style.height = 0;
+    node.style.transition = `height ${duration}ms cubic-bezier(0.700, 0.270, 0.270, 1)`;
+    node.style.overflow = 'hidden';
+
+    requestAnimationFrame(() => {
+        node.style.height = height;
+    });
+
+    node.addEventListener('transitionend', () => {
+        node.style.height = '';
+        node.style.transition = '';
+        node.style.overflow = '';
+        if (callback) callback();
+    }, {
+        once: true
+    });
+}
+
+const slideUp = (node, duration, callback) => {
+    if (isHidden(node)) return;
+    node.style.height = node.scrollHeight + 'px';
+    node.style.transition = `height ${duration}ms cubic-bezier(0.700, 0.270, 0.270, 1)`;
+    node.style.overflow = 'hidden';
+
+    requestAnimationFrame(() => {
+        node.style.height = 0;
+    });
+
+    node.addEventListener('transitionend', () => {
+        node.style.height = '';
+        node.style.transition = '';
+        node.style.overflow = '';
+        node.style.display = 'none';
+        if (callback) callback();
+    }, {
+        once: true
+    });
+}
+
+const ß = (node, element) => {
+    const obj = element || document;
+    const qs = obj.querySelectorAll(node);
+    return Array.from(qs);
+}
+
+const toggle = () => {
+    const obj = '.js-toggle';
+    if (!exists(obj)) return;
+    ß(obj).map((el) => {
+        el.onclick = () => el.classList.toggle('is-active');
+    });
+}
+
+const clearSession = () => {
+    const obj = '.js-clear-session';
+    if (!exists(obj)) return;
+    ß(obj).map((el) => {
+        el.onclick = () => sessionStorage.clear();
+    });
+}
+
+const button = () => {
+    const obj = '.button';
+    if (!exists(obj)) return;
+    ß(obj).map((el) => {
+        el.onclick = () => {
+            if (el.hasAttribute('data-loader')) el.classList.toggle('is-loading');
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
     toggle();
     clearSession();
     button();
 });
 
-
-
 // preloader
 // ------------------------------------------------------------	
-function loader() {
-    var loader = '.js-loader';
-    var content = '.js-loaded';
+const loader = () => {
+    const loader = '.js-loader';
+    if (!exists(loader)) return;
 
-    if (loader[0]) {
-        // init loader view
-        TweenLite.to(loader, 0.1, {
-            opacity: 1
+    window.addEventListener('load', function () {
+        TweenLite.to(loader, 0.3, {
+            delay: 0.7,
+            ease: Power3.easeInOut,
+            autoAlpha: 0,
+            display: 'none',
         });
-
-        // load website
-        window.addEventListener('load', function () {
-            TweenLite.to(loader, 0.3, {
-                delay: 0.7,
-                ease: Power3.easeInOut,
-                autoAlpha: 0,
-                display: 'none',
-                onComplete: function () {
-                    TweenLite.set(content, {
-                        display: 'block',
-                    });
-                }
-            });
-        });
-    }
+    });
 }
 loader();
