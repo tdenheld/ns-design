ef0013185a81fb2d724f4d7118821db7515477a0
Tibor den Held <tibor.denheld@ns.nl>
2019-10-29 22:20:17 +0100

    cleanup

--- assets/js/selector.js
+++ assets/js/selector.js
@@ -2,19 +2,18 @@
 
 (() => {
     const time = 250;
-    const obj = ".js-selector-item";
-    const toggleClass = "is-selected";
-    const initID = "#js-selector-item-0"; // set default on current card
-    const content = ".js-selector-content";
-    let paynow = true;
+    const obj = '.js-selector-item';
+    const toggleClass = 'is-selected';
+    const initID = '#js-selector-item-0'; // set default on current card
+    const content = '.js-selector-content';
 
     if ($(obj)[0]) return
     $(obj).each(function (i) {
-        $(this).attr("id", "js-selector-item-" + i);
+        $(this).attr('id', 'js-selector-item-' + i);
     });
 
     $(content).slideUp(time);
-    $(initID + " " + content).slideDown(0);
+    $(initID + ' ' + content).slideDown(0);
     $(initID).addClass(toggleClass);
 
     $(obj).click(function () {
