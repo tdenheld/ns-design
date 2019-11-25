b7fa4700f288090f2dce2021f8206e7c91053eb2
Tibor den Held <tibor.denheld@ns.nl>
2019-11-25 12:16:53 +0100

    update loader

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -5,9 +5,10 @@
     if (!exists(loader)) return;
 
     window.addEventListener('load', function () {
-        TweenLite.to(loader, 0.3, {
+        gsap.to(loader, {
+            duration: 0.3,
             delay: 0.7,
-            ease: Power3.easeInOut,
+            ease: 'power3.inOut',
             autoAlpha: 0,
             display: 'none',
         });
